03d022905b07043a1a1297b5f7e2d9fc236ff6de
Yukiya Takagi <yukiya@tonchidot.com>
2014-10-19 14:59:04 +0900

    Initial Commit

---
+++ SwiftPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftPractice
+//
+//  Created by Yukiya Takagi on 2014/10/19.
+//  Copyright (c) 2014年 Yukiya Takagi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
